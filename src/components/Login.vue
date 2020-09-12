49d160b44c89a709d9836177972772f6a9ddc7ab
wj <wj@gmail.com>
2020-09-12 17:40:13 +0800

    login UI

---
+++ src/components/Login.vue
@@ -0,0 +1,100 @@
+<template>
+  <div class="login_container">
+      <div class="login_box">
+          <div class="avatar_box">
+              <img src="../assets/logo.png">
+          </div>
+          <el-form ref="loginFormRef" :model="loginForm" :rules="loginFormRules" label-width="0px" class="login_form">
+            <el-form-item prop="username">
+                <el-input v-model="loginForm.username" prefix-icon="el-icon-s-custom"></el-input>
+            </el-form-item>
+            <el-form-item prop="password">
+                <el-input type="password" v-model="loginForm.password" prefix-icon="el-icon-lock"></el-input>
+            </el-form-item>
+            <el-form-item class="btns">
+                <el-button type="primary">登录</el-button>
+                <el-button type="info" @click="reset">重置</el-button>
+            </el-form-item>
+          </el-form>
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      loginForm: {
+        username: '',
+        password: ''
+      },
+      loginFormRules: {
+        username: [
+          { required: true, message: '请输入名称', trigger: 'blur' },
+          { min: 3, max: 5, message: '长度在 3 到 5 个字符', trigger: 'blur' }
+        ],
+        password: [
+          { required: true, message: '请输入密码', trigger: 'blur' },
+          { min: 5, max: 8, message: '长度在 5 到 8 个字符', trigger: 'blur' }
+        ]
+      }
+    }
+  },
+  methods: {
+    reset () {
+      this.$refs.loginFormRef.resetFields()
+    }
+  }
+}
+</script>
+
+<style scoped>
+.login_container {
+    background-color: #2b4b6b;
+    height: 100%;
+}
+
+.login_box {
+    width: 450px;
+    height: 300px;
+    background-color: #fff;
+    border-radius: 3px;
+    position: absolute;
+    left: 50%;
+    top: 50%;
+    transform: translate(-50%, -50%);
+}
+
+.avatar_box {
+    width: 130px;
+    height: 130px;
+    border: 1px solid #eee;
+    border-radius: 50%;
+    padding: 10px;
+    box-shadow: 0 0 10px #ddd;
+    position: absolute;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    background-color: #eee;
+}
+
+.avatar_box img {
+    width: 100%;
+    height: 100%;
+    border-radius: 50%;
+    background-color: #eee;
+}
+
+.btns {
+    display: flex;
+    justify-content: flex-end;
+}
+
+.login_form {
+    position: absolute;
+    bottom: 0;
+    width: 100%;
+    padding: 0 20px;
+    box-sizing: border-box;
+}
+</style>
